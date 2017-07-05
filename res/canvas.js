18475f1da61a0d84753c581ff3a170d1378bf7d9
briannoyama <briannoyama@gmail.com>
2017-07-04 22:57:43 -0500

    Linear gradient. Starting script for link animations.

--- res/canvas.js
+++ res/canvas.js
@@ -5,6 +5,7 @@
 var canvas = (function(colors){
 	var window_ = $(window);
 	var body = document.getElementById('page');
+	var title = document.getElementById('title');
 	var menu = document.getElementById('menu');
 	//The element that we will put polygons in.
 	var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
@@ -14,7 +15,6 @@ var canvas = (function(colors){
 	body.appendChild(svg);
 
 	var height = svg.getBoundingClientRect().height*50/260;
-	var menu_offset = menu.offsetTop;
 	window_.scroll(function(){
 		var w_offset = window_.scrollTop();
 		if (w_offset > height){
@@ -23,17 +23,16 @@ var canvas = (function(colors){
 			svg.setAttribute('style','top: 0; bottom: auto;');
 		}
 
-		if (w_offset > menu_offset){
-			menu.setAttribute('style','position: fixed;');
+		if (w_offset > title.offsetHeight){
+			menu.setAttribute('style','position: fixed; background:linear-gradient(#FFFFFFFF, #FFFFFF00);');
 		} else {
-			menu.setAttribute('style','position: relative;');
+			menu.setAttribute('style','position: relative; background: auto;');
 		}
 
 	});
 
 	window_.resize(function(){
 		height = svg.getBoundingClientRect().height*50/260;
-		menu_offset = menu.offsetTop;
 	});
 
 	return {
---
+++ res/circlelinks.js
@@ -0,0 +1,10 @@
+var circlelinks(function(){
+	var circles = document.getElementsByClassName("circle"); 
+	var circles_length = circles.length;
+	for (var i = 0; i < circles_length; i++){
+		//TODO add animations for links here.
+
+	}
+
+
+});
