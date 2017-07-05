c03d7f252f138f17646d790865e5448b6827ae32
briannoyama <briannoyama@gmail.com>
2017-07-04 19:25:41 -0500

    Menu scrolls with screen, and subtitle.

--- res/canvas.js
+++ res/canvas.js
@@ -5,6 +5,7 @@
 var canvas = (function(colors){
 	var window_ = $(window);
 	var body = document.getElementById('page');
+	var menu = document.getElementById('menu');
 	//The element that we will put polygons in.
 	var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 	//TODO Change the viewBox based off of the size of the parent element.
@@ -12,16 +13,27 @@ var canvas = (function(colors){
 	svg.setAttribute('id','origami');
 	body.appendChild(svg);
 
-
+	var height = svg.getBoundingClientRect().height*50/260;
+	var menu_offset = menu.offsetTop;
 	window_.scroll(function(){
-		var w_offset = $(window).scrollTop();
-		var height = svg.getBoundingClientRect().height*50/260;
+		var w_offset = window_.scrollTop();
 		if (w_offset > height){
-			//Math.round(w_offset)
 			svg.setAttribute('style','top: auto; bottom: 0;');
 		} else {
 			svg.setAttribute('style','top: 0; bottom: auto;');
 		}
+
+		if (w_offset > menu_offset){
+			menu.setAttribute('style','position: fixed;');
+		} else {
+			menu.setAttribute('style','position: relative;');
+		}
+
+	});
+
+	window_.resize(function(){
+		height = svg.getBoundingClientRect().height*50/260;
+		menu_offset = menu.offsetTop;
 	});
 
 	return {
