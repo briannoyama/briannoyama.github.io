7a7c64fd3d476506a2e225280da5b9f5ed786b64
Brian Noyama <brian.noyama@gmail.com>
2017-08-12 10:22:42 -0500

    Started education and experience sections.

---
+++ assets/res/venn_edu.js
@@ -0,0 +1,16 @@
+
+
+var venn_edu(function(venn_obj, script){
+
+
+  var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svg.setAttribute('xmlns:xlink', 'http://www.w3.org/1999/xlink');
+  svg.setAttribute('viewBox', '0 0 10 6');
+  svg.setAttribute('class', 'education');
+
+
+  script.parentNode.appendChild(svg);
+  
+
+
+});
